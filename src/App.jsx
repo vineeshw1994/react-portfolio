f27459dd30d6b91d61eeeee841d47f27929519a8
vineesh <110625835+vineeshw1994@users.noreply.github.com>
2023-10-20 23:59:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import {BrowserRouter} from 'react-router-dom'
 
 import{About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from "./components";
-
+//use context api here and this is for the state passing
 const App =()=> {
 
   return (
